768e6fd11663bb3174488c0911920d9c69d94bcc
rhctmxk <rhctmxk@naver.com>
2025-05-12 11:45:01 +0900

    [FIX] Hardware Images 안보임 에러

--- src/middleware.ts
+++ src/middleware.ts
@@ -82,7 +82,7 @@ export const config = {
         // '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|images/|fonts/|video/).*)',
         // '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|images/|fonts/|video/).*)',
         '/',
-        '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|images/|fonts/|video/|auth|pdf/).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|images/|fonts/|video/|auth|pdf/|images/).*)',
 
     ],
 }
