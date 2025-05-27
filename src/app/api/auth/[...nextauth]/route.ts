7e50bb76ac32c7384de9ae748c9aa428b7b9874e
rhctmxk <rhctmxk@naver.com>
2025-05-27 14:47:14 +0900

    [Build]

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,44 @@
 // app/api/auth/[...nextauth]/route.ts
-import { RequestInternal } from 'next-auth'
 import NextAuth from 'next-auth/next'
-import { User } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import KakaoProvider from "next-auth/providers/kakao";
-import prisma from '@/app/lib/prisma'
-import * as bcrypt from 'bcrypt'
 
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
             name: 'Credentials',
             credentials: {
-                username: { label: '이메일', type: 'text' },
+                username: { label: '이메일', type: 'text', placeholder: '이메일 주소를 입력해 주세요.' },
                 password: { label: '비밀번호', type: 'password' },
             },
-            async authorize(
-                credentials: Record<'username' | 'password', string> | undefined,
-                req: Pick<RequestInternal, 'method' | 'body' | 'headers' | 'query'>
-            ): Promise<User | null> {
-                if (!credentials?.username || !credentials?.password) return null;
+            async authorize(credentials, req) {
+                const res = await fetch(`${process.env.NEXTAUTH_URL}/api/signin`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        username: credentials?.username,
+                        password: credentials?.password,
+                    }),
+                })
+                const user = await res.json()
+                console.log('$$$user: ', user)
 
-                const user = await prisma.user.findUnique({
-                    where: { email: credentials.username },
-                });
+                if (user) {
+                    // 반환된 모든 객체는 JWT의 '사용자' 속성에 저장됩니다.
+                    return {
+                        id: user.id, // ✅ 이건 있어야 하고
+                        name: user.name, // ✅ 필요하다면 name도 추가
+                        email: user.email,
+                        role: user.role, // ✅ 핵심!
+                    };
+                } else {
+                    // null을 반환하면 사용자에게 세부 정보를 확인하라는 오류가 표시됩니다. null 반환
+                    return null
 
-                if (!user || !user.password) return null;
+                    // 이 콜백은 오류로 거부할 수도 있으므로 사용자는 오류 메시지를 쿼리 매개변수로 사용하여 오류 페이지로 전송됩니다.
+                }
 
-                const isValid = await bcrypt.compare(credentials.password, user.password);
-                if (!isValid) return null;
-
-                return {
-                    id: String(user.id),
-                    name: user.name ?? '관리자',
-                    email: user.email,
-                    role: user.role,
-                };
-            }
+            },
         }),
         // 카카오 프로바이더
         KakaoProvider({
@@ -56,17 +58,16 @@ const handler = NextAuth({
         },
 
         async session({ session, token }) {
-            // console.log('$$$ token: ', token)
+            console.log('$$$ token: ', token)
             session.user = {
                 name: token.name,
                 email: token.email,
                 role: token.role,
             };
-            // console.log('$$$ session: ', session)
+            console.log('$$$ session: ', session)
             return session;
         },
     },
-    debug: true,
     pages:{
         signIn: '/ko/auth/signin',
         signOut: '/ko/auth/signout',
