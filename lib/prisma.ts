74c4668033dc74969388bfd49634f55b11ab630b
rhctmxk <rhctmxk@naver.com>
2025-05-28 12:31:12 +0900

    [TEST] lib/prisma 코드 스타일 변경

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,13 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
-
 export const runtime = "nodejs"; // 👈 반드시 추가
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    });
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
