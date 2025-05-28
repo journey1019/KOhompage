9a5696a68d03d625d48a5fe3d451f97df5920561
rhctmxk <rhctmxk@naver.com>
2025-05-28 11:16:05 +0900

    [TEST] prisma & hardware data url 수정

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,20 +1,35 @@
 // /app/lib/prisma.ts
+export const runtime = "nodejs"; // 👈 반드시 추가
 
 // import { PrismaClient } from "@/generated/prisma";
 import { PrismaClient } from "@prisma/client";
 
-export const runtime = "nodejs"; // 👈 반드시 추가
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query'], // optional: 쿼리 로깅
-    })
-
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
+
+
+// // /app/lib/prisma.ts
+//
+// // import { PrismaClient } from "@/generated/prisma";
+// import { PrismaClient } from "@prisma/client";
+//
+// export const runtime = "nodejs"; // 👈 반드시 추가
+//
+// const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+//
+// // export const prisma = globalForPrisma.prisma || new PrismaClient();
+// export const prisma =
+//     globalForPrisma.prisma ??
+//     new PrismaClient({
+//         log: ['query'], // optional: 쿼리 로깅
+//     })
+//
+//
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+//
+// export default prisma;
