b888cf47bd673e7a849d79c505dda431253a9a9b
rhctmxk <rhctmxk@naver.com>
2025-05-28 15:57:52 +0900

    [TEST] Prisma Client를 다시 생성해보자

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -15,13 +15,41 @@
 //
 // export default prisma;
 
-export const runtime = "nodejs"; // 👈 반드시 추가
 
-// import { PrismaClient } from "@/generated/prisma";
-import { PrismaClient } from "@prisma/client";
+
+
+
+
+// export const runtime = "nodejs"; // 👈 반드시 추가
+//
+// // import { PrismaClient } from "@/generated/prisma";
+// import { PrismaClient } from "@prisma/client";
+//
+// const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+//
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
+//
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
+
+
+
+
+
+
+
+// lib/prisma.ts 예시 (Next.js App Router 기준)
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
